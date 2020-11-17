fcc24bb9153f1d8a8b61c308af22984491cdb4c4
deep073 <deepkmr073@gmail.com>
2020-11-17 19:18:22 +0530

    calculator

---
+++ Cm Calculator/My Project/AssemblyInfo.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Globalization
+Imports System.Resources
+Imports System.Windows
+<Assembly: AssemblyTitle("Cm_Calculator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Cm_Calculator")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: ComVisible(false)>
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)>
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
